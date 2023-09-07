8da8761b90429039087ba50d08414dc042892404
kiscean <visocorodnii@gmail.com>
2023-09-07 17:00:11 +0300

    Work 07-09

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {NavLink} from "react-router-dom";
 
 import {Layout, Space} from "antd";
+import logo from '../../assets/img/zaslon_logo_white.png';
 
 import './Header.css';
 
@@ -10,7 +11,14 @@ const Header = () => {
     return (
         <Header className="header">
             <Space direction="horizontal">
-                Header
+                <Space direction="horizontal">
+                    <a href="#">
+                        <img  src={logo} alt="main_logo" className="header__logo-link"/>
+                    </a>
+                    <span className="header__logo-description">
+                        <h2>ГОСТЕВОЙ МЕНЕДЖЕР</h2>
+                    </span>
+                </Space>
             </Space>
         </Header>
     );
