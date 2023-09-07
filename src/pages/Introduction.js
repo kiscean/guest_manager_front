9d2fa112fcaa6b991377725582f2f44d003ef31e
kiscean <visocorodnii@gmail.com>
2023-09-07 16:47:57 +0300

    Work 07-09

--- src/pages/Introduction.js
+++ src/pages/Introduction.js
@@ -4,13 +4,19 @@ import TableIntroreg from "../components/table_introreg/TableIntroreg";
 import RegForm from "../components/reg_form/RegForm";
 import QRcode_Generator from "../components/qrcode_generator/QrCodeGenerator";
 
-import './style.css';
+import {Col, Row} from "antd";
 
 const Introduction = () => {
     return (
         <div>
-            <RegForm />
-            <TableIntroreg />
+            <Row>
+                <Col xl={12} sm={24}>
+                    <RegForm />
+                </Col>
+                <Col xl={12} sm={24}>
+                    <TableIntroreg />
+                </Col>
+            </Row>
         </div>
     );
 }
